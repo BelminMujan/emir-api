a28c5979001163639510ddce5a441b454c6689cf
belmin.mujan <mujan.belmin98@gmail.com>
2022-03-02 13:20:49 +0100

    ..

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const cors = require("cors");
+
 require("dotenv").config();
 const db = require("./mongodb");
 const workflowRouter = require("./routes/workflow");
 const stageRouter = require("./routes/stage");
 const actionRouter = require("./routes/action");
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/workflow", workflowRouter);
